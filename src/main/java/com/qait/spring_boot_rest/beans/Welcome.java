63756905e5f366ad7c1f85f20b352535e6b99b24
Tanuj Vishnoi <tanujkumarvishnoi@qainfotech.com>
2018-09-10 15:44:57 +0530

    POST REQUEST

---
+++ src/main/java/com/qait/spring_boot_rest/beans/Welcome.java
@@ -0,0 +1,14 @@
+package com.qait.spring_boot_rest.beans;
+
+public class Welcome {
+	public Welcome(String message) {
+		super();
+		this.message = message;
+	}
+
+	private final String message;
+
+	public String getMessage() {
+		return message;
+	}
+}
---
+++ src/main/java/com/qait/spring_boot_rest/controller/getStudentDataController.java
@@ -0,0 +1,9 @@
+package com.qait.spring_boot_rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class getStudentDataController {
+
+	
+}
